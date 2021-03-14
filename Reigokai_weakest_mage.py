7378bcc0b3387d8a4e3841910f61d0f5ba7c695d
Charlie <35852547+charlies12@users.noreply.github.com>
2021-03-13 21:38:46 -0800

    Update Reigokai_weakest_mage.py

--- Reigokai_weakest_mage.py
+++ Reigokai_weakest_mage.py
@@ -5,77 +5,55 @@ import pandas as pd
 page = requests.get("https://isekailunatic.com/category/weakest-mage/")
 soup = BeautifulSoup(page.content, 'html.parser')
 
+# Searches main page for chapters
 title = soup.find(id="main")
-# chapters = title.find_all(class_="entry-title")
-chapters = title.find_all(class_="entry-header")
-current_chapters = chapters[0]
-# print(current_chapters)
-# for chapter in chapters:
-#     print(chapter)
-# current_chapters = chapters
+title_items = title.find_all(class_="entry-title")
+current = title_items[0]
 
-show = current_chapters.find("a")
-desc = show['href']
+# Date posted on 
+posted_on = title.find_all(class_="entry-meta")
+current_post = posted_on[0]
 
-# print(show)
-# print(desc)
+# Selects date
+date_tag = title.select("time.entry-date")
+simple_date = [dts.get_text() for dts in date_tag]
 
-entry_title = title.select(".entry-title")
-# link = title.select("href")
-chapter_title = [ct.get_text() for ct in entry_title]
-# periods2 = [pt2.get_text() for pt2 in link]
-# print(chapter_title)
+# Searches for url
+search_url = title.select(".entry-title a[href]")
+url_link = [ul.get('href') for ul in search_url]
 
-# short_descs = [sd.get_text() for sd in title.select(".site-content .href ")]
-# temps = [t.get_text() for t in seven_day.select(".tombstone-container .temp")]
-# descs = [d["title"] for d in seven_day.select(".tombstone-container img")]
+# Searches for chapter number and title
+chapter_header = title.select("h1.entry-title")
+tag = [ch.get_text() for ch in chapter_header]
 
-
-show_chapters = pd.DataFrame(
+chapter_details = pd.DataFrame(
     {
-        "Chapter": chapter_title#,
-        #"Link": periods2
-
+        "Title": tag,
+        "URL": url_link,
+        "Posted-on": simple_date
     }
 )
 
-
-print(show_chapters)
-
-
-
-# show_chapters.to_csv('weakest_mage.csv', index=False)
+print(chapter_details)
 
 
 
+# Things that didn't work how I wanted
+
+# This only searches 1 link. Not used, but will leave here.
+# url_description = current.find('a')
+# url_short_description = url_description['href']
+
+# Searches URLs that start with 'https'
+# Actually, this doesn't do what I want it to do. I'll leave it here.
+# link_description = title.select("a[href^='https']")
+# just_link = [ld.get_text() for ld in link_description]
+
+# column alignment and column width
+# pd.set_option('display.max_colwidth', 100)
+# chapter_details.style.set_properties(subset=["col1", "col2"], **{'text-align': 'left'})
 
 
 
-
-
-# print(current_chapters.prettify())
-
-# chapter_title = current_chapters.find(class_="entry-header").get_text()
-# chapter_link = current_chapters.find(a="href").get_text()
-#
-# print(chapter_title)
-# print(chapter_link)
-
-# img = current_chapters.find()
-
-# show_me_the_tags = soup.find_all('h1', class_='entry-title')
-
-# print(show_me_the_tags)
-
-'''for tag in show_me_the_tags:
-    print(tag)'''
-
-# print(list(soup.children))
-# info = [type(item) for item in list(soup.children)]
-# print(info)
-
-# html = list(soup.children)[2]
-
-# print(list(html.children))
 
 
